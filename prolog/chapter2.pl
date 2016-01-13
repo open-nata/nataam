20a7c05174fbb7217bcc49b2f118c06f8d70afb6
rdmclin2 <rdmclin2@163.com>
2016-01-13 13:49:50 +0800

    add gradle

---
+++ prolog/chapter2.pl
@@ -0,0 +1,11 @@
+male(di).
+male(jianbo).
+female(xin).
+female(yuan).
+female(yuqing).
+father(jianbo,di).
+father(di,yuqing).
+mother(xin,di).
+grandfather(X,Y):-father(X,Z),father(Z,Y).
+grandmother(X,Y):-mother(X,Z),father(Z,Y).
+daughter(X,Y):-father(X,Y),female(Y).
